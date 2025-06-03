4b472caa971c8fbeb2797b58b73e00caaba910ad
josejesus823 <80081347+sebas102537@users.noreply.github.com>
2025-06-03 09:50:52 -0500

    Estas son las variables en clase

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,44 @@
 package org.example;
 
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
 
-       
+        System.out.println("Reserva App");
+        System.out.println("===================\n");
+
+        Scanner entradaTeclado=new Scanner(System.in);
+        LocalDate fechaReserva;
+        LocalDate fechaMantenimiento;
+        String nombreUsuario;
+        String idUsuario;
+        String correoUsuario;
+        String tipoUsuario;
+        String numeroApartemento;
+        String tipoApartamento;
+        String contraseñaUsuario;
+        String telefonoUsuario;
+
+        String horarioEspacio;
+        String nombreEspacio;
+        String reglamentoEspacio;
+        String utensiliosEspacio;
+
+        String codigoReserva;
+        String horarioReserva;
+
+        byte numeroBloque;
+        byte cantidadAsistentes;
+        byte edadUsuario;
+        int valorReserva;
+
+        boolean esAdministrador=false;
+        boolean confirmacionReserva=true;
+        boolean estaOcupado=false;
+        boolean estaEnMantenimiento=false;
         }
     }
-}
